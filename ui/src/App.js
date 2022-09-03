91156077a7ac44f708d9dc4a834359549e76ac2a
tomo0119shige <tomo0119shige@gmail.com>
2022-09-03 16:53:58 +0900

    selectbox

--- ui/src/App.js
+++ ui/src/App.js
@@ -13,44 +13,46 @@ function App() {
 
   const [msgs, setMsgs] = useState([])
   const [post, setpost] = useState({})
-  const [apiRootUrl, setApiRootUrl] = useState({})
+  const [apiRootUrl, setApiRootUrl] = useState()
   const inputNameRef = useRef()
   const inputTextRef = useRef()
-  const inputApiurlRef = useRef()
 
   const urllist = [
-    "http://localhost:8000",
-    "http://localhost:9000",
+    {value: '', label: 'Choose Host'},
+    {value: 'http://localhost:8000', label: 'localhost'},
+    {value: 'http://localhost:9000', label: 'cloud'},
   ]
 
   const handleName = event => {
     setpost({username:event.target.value, text: post.text})
   }
 
-  const handleApiRootUrl = event => {
-    setApiRootUrl({apiRootUrl: event.target.value})
-  }
-
   const handleText = event => {
     setpost({username: post.username, text: event.target.value})
   }
 
+  const handleApiRootUrl = event => {
+    setApiRootUrl(event.target.value)
+    setMsgs([{"created_at": "loading"}])
+  }
+
   useEffect(() => {
     const interval = setInterval(() => {
-      const apiUrl = apiRootUrl + "/messages/"
-      axios.get(apiUrl)
-      .then(res => {
-        console.log(res)
-        setMsgs(res.data)
-      })
+      console.log(apiRootUrl + "/messages/")
+      if (apiRootUrl) {
+        axios.get(apiRootUrl + "/messages/")
+        .then(res => {
+          console.log(res)
+          setMsgs(res.data)
+        })
+      }
     }, 1000);
     return () => clearInterval(interval);
-  }, [])
+  }, [apiRootUrl])
 
   const clickSubmit = (e) => {
     // POST処理
-    const apiUrl = apiRootUrl + "/messages/"
-    axios.post(apiUrl, post)
+    axios.post(apiRootUrl + "/messages/", post)
     .then(res => {
       console.log(res)
       setMsgs(res.data)
@@ -72,24 +74,26 @@ function App() {
     }}>
       <h1>HTTP CHAT <small><small>by iwsh</small></small></h1>
       <div>
-        <table cellPadding={5}>
-          <tr>
-            <td>Name:</td>
-            <td><input ref={inputNameRef} type="text" onChange={handleName} required/></td>
-          </tr>
-          <tr>
-            <td valign="middle">Message:</td>
-            <td><textarea ref={inputTextRef} cols="40" rows="5" onChange={handleText} required/></td>
-          </tr>
-          <button type="submit" onClick={clickSubmit}><big>Submit</big></button>
-        </table>
-      </div>
-      <hr></hr>
-      <div>
-        <select name="ap" defaultValue="UTC" ref={inputTimezoneRef} onChange={handleTimezone}>
+        <select onChange={handleApiRootUrl}>
           {urllist.map( u => <option value={u.value}>{u.label}</option>)}
         </select>
       </div>
+      {(apiRootUrl)?
+        <div>
+          <table cellPadding={5}>
+            <tr>
+              <td>Name:</td>
+              <td><input ref={inputNameRef} type="text" onChange={handleName} required/></td>
+            </tr>
+            <tr>
+              <td valign="middle">Message:</td>
+              <td><textarea ref={inputTextRef} cols="40" rows="5" onChange={handleText} required/></td>
+            </tr>
+            <button type="submit" onClick={clickSubmit}><big>Submit</big></button>
+          </table>
+        </div>
+      :<div></div>}
+      <hr></hr>
       {msgs.map(msg => {
         return (
           <p>
