3791a2cbec914b86672749cbeeabf2eea0ecbf5d
iwsh <koabuwakukiro@gmail.com>
2023-03-04 21:54:09 +0900

    Add modal for notifications

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 
 import { Route, Navigate, Routes, BrowserRouter } from 'react-router-dom';
+import NotificationModal from './components/Modal';
 import { is_authenticated } from './contexts/AuthContext';
+import ModalContext from './contexts/ModalContext';
 
 const Login = React.lazy(() => import('./pages/Login'));
 const Chat = React.lazy(() => import('./pages/Chat'));
 
 export default function App() {
+  const isModalShown = React.useState(false);
+  const modalTitle = React.useState("initial title");
+  const modalMessage = React.useState("initial message");
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/' element={
-          <PrivateRoute children={<Navigate to='/chat' />} />
-        } />
-        <Route path='/chat' element={
-          <PrivateRoute children={<Chat />} />
-        } />
-      </Routes>
-    </BrowserRouter>
+    <ModalContext.Provider value={{ isModalShown, modalTitle, modalMessage }}>
+      <NotificationModal />
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={
+            <PrivateRoute children={<Navigate to='/chat' />} />
+          } />
+          <Route path='/chat' element={
+            <PrivateRoute children={<Chat />} />
+          } />
+        </Routes>
+      </BrowserRouter>
+    </ModalContext.Provider>
   );
 }
 
--- ui/src/pages/Login.js
+++ ui/src/pages/Login.js
@@ -1,18 +1,24 @@
 import '../App.css';
 import logo from '../logo.svg';
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useContext, useRef } from "react";
 import { useNavigate } from "react-router";
 import { login } from '../contexts/AuthContext';
 import { backend_api } from "../helper/ApiHelper";
+import ModalContext from '../contexts/ModalContext';
 
 
 export default function Login() {
   const navigate = useNavigate();
-  const [error, setError] = useState("");
 
   const [message, setMessage] = useState("")
   const github_client_id = process.env.REACT_APP_GITHUB_CLIENT_ID
   const github_oauth_url = `https://github.com/login/oauth/authorize?client_id=${github_client_id}&scope=user:read`
+  const setModalIsShown = useContext(ModalContext).isModalShown[1];
+  const setmodalTitle = useContext(ModalContext).modalTitle[1];
+  const setModalMessage = useContext(ModalContext).modalMessage[1];
+
+  // For develop environment with strictmode
+  const refFirstRef = useRef(true);
 
   const loginAndNavigate = useCallback(() => {
     login();
@@ -37,20 +43,29 @@ export default function Login() {
       })
         .then(response => {
           console.log(response.data);
-          setError("");
           setMessage(response.data.message);
           loginAndNavigate();
         })
         .catch(error => {
-          // useEffectが2回実行され、片方が401となるのでエラーの表示を遅らせる
-          setTimeout(() => {
-            setError("Cannot login. Try again.");
-            setMessage("");
-            console.log(error);
-          }, 1000)
+          console.error(error);
+          // NOTE: When StrictMode is enabled, React intentionally performs
+          //       operations twice in devlopment. Login request is also
+          //       excuted twice though code is valid only once. We must ignore
+          //       first login error because one of the two responces is always error.
+          if (process.env.NODE_ENV === "development") {
+            if (refFirstRef.current) {
+              refFirstRef.current = false;
+              console.error("if");
+              return;
+            }
+          }
+          setModalIsShown(true);
+          setmodalTitle("Login Error");
+          setModalMessage("Cannot login. Try again.");
+          setMessage("");
         });
     }
-  }, [loginAndNavigate])
+  }, [loginAndNavigate, setModalIsShown, setmodalTitle, setModalMessage])
 
 
   return (
@@ -70,13 +85,6 @@ export default function Login() {
         >
           LOGIN with Github
         </a>
-        {
-          error ?
-            <div style={{ color: "lightsalmon" }}>
-              {error}
-            </div>
-            : null
-        }
       </header>
     </div>
   );
