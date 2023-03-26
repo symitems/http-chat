80a3e2bc5d3fe5592fb360ecdcca107b77e040e7
kegoly <510kei.goto@gmail.com>
2023-03-26 10:52:55 +0000

    Add table element and adjust the place of avatar

--- ui/src/components/MessageList.js
+++ ui/src/components/MessageList.js
@@ -3,40 +3,53 @@ import React from "react";
 export default function MessageList(props) {
     return (
         <div>
-            <div style={{
-                marginTop: 15,
-            }}>
-                <b>
-                    <img src={props.msg.avatar_url} alt="画像はありません" style={{
-                        width: "20px",
-                        height: "auto",
-                        }} />{" "}
-                    <big>{props.msg.username}</big>
-                </b>{" "}
-                <small> - [ {props.changeTimezone(props.msg.created_at)} ]</small>
-            </div>
-            <div
-                style={{
-                    backgroundColor: "#ffffff",
-                    padding: 5,
-                    marginLeft: "1%",
-                    marginTop: 5,
-                    marginRight: "5%",
-                    // maxWidth: '50%',
-                    alignSelf: "flex-end",
-                    borderRadius: 15,
-                    display: "inline-block",
-                    border: "1.5px solid",
-                    borderColor: "#b0e0e6",
-                }}
-            >
-                {props.msg.text.split('\n').map((line, i) => (
-                    <React.Fragment key={i}>
-                        {line}
-                        <br />
-                    </React.Fragment>
-                ))}
-            </div>
+            <table style={{ marginTop: 15, borderCollapse: "collapse" }}>
+                <tbody>
+                    <tr>
+                        <td rowSpan={2} valign="top">
+                            <img
+                                src={props.msg.avatar_url}
+                                alt="画像はありません"
+                                style={{
+                                    borderRadius: 50,
+                                    marginTop: 15,
+                                    width: "30px",
+                                    height: "auto",
+                                }}
+                            />
+                        </td>
+                        <td style={{ whiteSpace: "nowrap" }}>
+                            <b style={{ marginLeft: "1%" }}>{props.msg.username}</b>
+                            <small> - [ {props.changeTimezone(props.msg.created_at)} ]</small>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div
+                                style={{
+                                backgroundColor: "#ffffff",
+                                padding: 5,
+                                marginLeft: "1%",
+                                marginTop: 10,
+                                marginRight: "5%",
+                                alignSelf: "flex-end",
+                                borderRadius: 15,
+                                display: "inline-block",
+                                border: "1.5px solid",
+                                borderColor: "#b0e0e6",
+                                }}
+                            >
+                                {props.msg.text.split("\n").map((line, i) => (
+                                <React.Fragment key={i}>
+                                    {line}
+                                    <br/>
+                                </React.Fragment>
+                                ))}
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
